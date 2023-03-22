d0dd8851b79ff9d612b371fc84303fd8523f8cbd
Anna Sukhodolova <asukhodolova@solvd.com>
2023-03-22 18:08:14 +0300

    Updated tests

--- cypress/integration/simple.js
+++ cypress/integration/simple.js
@@ -3,6 +3,11 @@ describe('Simple', () => {
   const searchValue = 'Zebrunner';
 
   it('should be passed ', () => {
+    cy.task('log', 'Env variables:')
+    cy.task('log', process.env.REPORTING_RUN_ENVIRONMENT);
+    cy.task('log', process.env.REPORTING_PROJECT_KEY);
+    cy.task('log', process.env);
+
     cy.visit(url).contains('Google');
 
     console.log(`Performing search with value Zebrunner`);

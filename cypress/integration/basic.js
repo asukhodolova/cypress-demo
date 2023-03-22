7df49e291f39af63a6685557b32e0aece54aef99
Anna Sukhodolova <asukhodolova@solvd.com>
2023-03-22 17:31:12 +0300

    Added log of env vars

--- cypress/integration/basic.js
+++ cypress/integration/basic.js
@@ -1,35 +1,36 @@
 describe('Google search', () => {
-    const url = 'https://www.google.com/';
-    const searchValue = 'Zebrunner';
+  const url = 'https://www.google.com/';
+  const searchValue = 'Zebrunner';
 
-    it('Should pass', {'owner': 'asukhodolova'}, () => {
-      // cy.zebrunnerTestCaseKey(undefined, 'ANNAS-1', 'ANNAS-5', 'ANNAS-6', null, "unexisting");
-      cy.visit(url).contains('Google');
-  
-      console.log(`Performing search with value Zebrunner`);
-      cy.xpath("//input[@name='q']").click().type(searchValue).type('{enter}');
-  
-      console.log(`Verify first search result contains search value`);
-      cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
-    });
-  
-    it('Should fail', {'owner': 'szagriychuk'}, () => {
-      // cy.zebrunnerTestCaseKey('ANNAS-2', 'ANNAS-4');
-      // cy.zebrunnerTestCaseStatus('ANNAS-2', 'RETEST');
-
-      cy.visit(url).contains('Google');
-  
-      console.log(`Performing search with value Zebrunner`);
-      cy.xpath("//input[@name='q']").click().type(searchValue).type('{enter}');
-  
-      console.log(`Verify first search result contains search value`);
-      cy.xpath("//*[@id='search']//a").should('contain.text', "asdalsdjalskdjlaksjdlkajsd");
-    });
-  
-    it.only('Empty test', () => {
-        // cy.zebrunnerTestCaseKey('ANNAS-3');
-        cy.task('log', 'Env variables:')
-        cy.task('log', process.env)
-    });
+  it('Empty test', () => {
+    // cy.zebrunnerTestCaseKey('ANNAS-3');
+    cy.log(process.env.REPORTING_RUN_ENVIRONMENT);
+    cy.log(process.env.REPORTING_PROJECT_KEY);
+    cy.log(process.env);
   });
   
+  it('Should pass', { 'owner': 'asukhodolova' }, () => {
+    // cy.zebrunnerTestCaseKey(undefined, 'ANNAS-1', 'ANNAS-5', 'ANNAS-6', null, "unexisting");
+    cy.visit(url).contains('Google');
+
+    console.log(`Performing search with value Zebrunner`);
+    cy.xpath("//input[@name='q']").click().type(searchValue).type('{enter}');
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  });
+
+  it('Should fail', { 'owner': 'szagriychuk' }, () => {
+    // cy.zebrunnerTestCaseKey('ANNAS-2', 'ANNAS-4');
+    // cy.zebrunnerTestCaseStatus('ANNAS-2', 'RETEST');
+
+    cy.visit(url).contains('Google');
+
+    console.log(`Performing search with value Zebrunner`);
+    cy.xpath("//input[@name='q']").click().type(searchValue).type('{enter}');
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', "asdalsdjalskdjlaksjdlkajsd");
+  });
+
+});

067c57da7866537873a51a91228abbce15ca1d61
Anna Sukhodolova <asukhodolova@solvd.com>
2023-05-24 12:35:26 +0300

    Updated tests

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -12,14 +12,13 @@ describe('Google search', () => {
         cy.visit(url).contains('Google');
         cy.wait(30000);
         cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
-        cy.xpath("//*[@id='search']//a").should('contain.text', "Carina");
+        cy.xpath("//*[@id='search']//a").should('contain.text', "hello-hello");
     });
 
     function performSearch(searchValue) {
         cy.visit(url).contains('Google');
         cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
         cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
-        cy.screenshot('my-screenshot' + searchValue);
         cy.wait(30000);
     }
 });

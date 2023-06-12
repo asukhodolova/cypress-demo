fab6a67568b242d6380bc09f4e7a90dc4bc0c03f
Anna Sukhodolova <asukhodolova@solvd.com>
2023-06-12 14:09:23 +0300

    Fast test check

--- cypress/e2e/simple.cy.js
+++ cypress/e2e/simple.cy.js
@@ -2,17 +2,7 @@ describe('Simple', () => {
   const url = 'https://www.google.com/';
   const searchValue = 'Zebrunner';
 
-  it('should be passed', () => {
-    cy.task('log', 'Env variables from test:')
-    cy.task('log', process.env);
-
-    cy.visit(url).contains('Google');
-
-    console.log(`Performing search with value Zebrunner`);
-    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
-
-    console.log(`Verify first search result contains search value`);
-    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  it('empty test', () => {
   });
 
   it('should be failed', () => {

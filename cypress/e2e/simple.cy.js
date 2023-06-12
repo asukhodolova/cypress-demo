737ecf99aa1bb28ddd0e37fa1398ef4df850a1c0
Anna Sukhodolova <asukhodolova@solvd.com>
2023-06-12 14:22:19 +0300

    1.1.2-dev17

--- cypress/e2e/simple.cy.js
+++ cypress/e2e/simple.cy.js
@@ -2,6 +2,19 @@ describe('Simple', () => {
   const url = 'https://www.google.com/';
   const searchValue = 'Zebrunner';
 
+  it('should be passed', () => {
+    cy.task('log', 'Env variables from test:')
+    cy.task('log', process.env);
+
+    cy.visit(url).contains('Google');
+
+    console.log(`Performing search with value Zebrunner`);
+    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  });
+
   it('empty test', () => {
   });
 
